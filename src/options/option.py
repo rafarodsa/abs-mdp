f00b79751191e7f0b8e991896d64f2adad287d00
Rafael Rodriguez <rrs@brown.edu>
2022-11-03 16:13:16 -0400

    reimplemented options

--- src/options/option.py
+++ src/options/option.py
@@ -8,17 +8,23 @@ import numpy as np
 
 
 class Option:
-    def __init__(self, initiation_classifier, policy_func, termination_prob, name=''):
+    def __init__(self, initiation_classifier, policy_func_factory, termination_prob, name=''):
         self.initiation = initiation_classifier
-        self.policy = policy_func
+        self.policy = policy_func_factory
+        self.executing = False
         self.termination_prob = termination_prob
         self.name = name
     
-    def is_executable(self, state):
-        return self.initiation(state)
+    def execute(self, state):
+        self.executing = self.initiation(state)
+        return self.executing
+
+    def is_executing(self):
+        return self.executing
 
     def act(self, state):
-        return self.policy(state), np.random.binomial(1, self.termination_prob(state)) == 1
+        self.executing = np.random.binomial(1, self.termination_prob(state)) == 1
+        return self.policy(state) if self.executing else None
     
     def __repr__(self) -> str:
         return self.name
