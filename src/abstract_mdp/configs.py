ee4f93084d123c0e2e9f7fdecf712450b1c65c04
Rafael Rodriguez <rrs@brown.edu>
2023-02-04 18:41:09 -0500

    structured configs definitions added

---
+++ src/abstract_mdp/configs.py
@@ -0,0 +1,69 @@
+from dataclasses import dataclass, field
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from omegaconf import OmegaConf, MISSING, DictConfig
+
+from src.models.configs import MLPConfig, DistributionConfig
+
+# Register a resolver with OmegaConf to handle math within .yaml files
+#
+# example.yaml:
+# ```
+# one: 1
+# two: 2
+# three: "${eval: ${one} + ${two}}"  # => 3
+# ```
+OmegaConf.register_new_resolver("eval", eval, replace=True)
+
+
+@dataclass
+class VAELossConfig:
+    grounding_const: float = 10
+    kl_const: float = 0.1
+    reward_const: float = 1.
+    transition_const: float = 0.1
+    init_class_const: float = 1.
+    n_samples: int = 1
+
+@dataclass
+class DataConfig:
+    data_path: str = MISSING
+    batch_size: int = 32
+    num_workers: int = 4
+    train_split: float = 0.8
+    val_split: float = 0.1
+    test_split: float = 0.1
+    shuffle: bool = True
+
+
+@dataclass
+class AbstractMDPConfig:
+    encoder: Optional[DistributionConfig]
+    decoder: Optional[DistributionConfig]
+    transition: Optional[DistributionConfig]
+    reward: Optional[Any]
+    init_class: Optional[Any]
+    gamma: Optional[Any]
+    n_options: int = MISSING
+    obs_dims: int = MISSING
+    latent_dim: int = MISSING 
+
+@dataclass
+class TrainerConfig:
+    experiment_cwd: Optional[str] = field(default='')
+    lr: float = 1e-4
+    batch_size: int = 32
+    epochs: int = 10
+    accelerator: str = "cpu"
+    devices: int = 1
+    save_path: str = "mdps"
+    loss: VAELossConfig = MISSING
+    model: AbstractMDPConfig = MISSING
+    data: DataConfig = MISSING
+
+
+@dataclass
+class ExperimentConfig:
+    name: str = MISSING
+    trainer: TrainerConfig = TrainerConfig()
+    seed: int = 0
