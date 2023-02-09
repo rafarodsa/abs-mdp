64c6ddc052e14c55ba8e81ec56f7d9456c6c34f8
Rafael Rodriguez <rrs@brown.edu>
2023-02-09 13:16:22 -0500

    refactor env

---
+++ envs/pinball/controllers_pinball.py
@@ -0,0 +1,166 @@
+"""
+    Options for Pinball Environments
+    author: Rafael Rodriguez-Sanchez
+    date: October 2022 
+    email: rrs@brown.edu
+
+"""
+import numpy as np
+from itertools import tee
+from scipy.special import erf, softmax
+from src.options.option import Option
+from functools import partial
+from gym import spaces
+### Control Position
+
+### Initiation Sets and Terminations
+def initiation_set(pinball_environment, distance):
+    def __initiation(state):
+        return not ball_collides(pinball_environment, state[:2], state[:2] + distance)
+    return __initiation
+
+
+def ball_collides(pinball_env, initial_position, final_position):
+    """ 
+        Detects collision at position
+    """
+    for obstacle in pinball_env.get_obstacles():
+        if _intersect_obstable(obstacle, initial_position, final_position):
+            return True
+    return False
+        
+
+def _intersect_obstable(obstacle, initial_position, final_position):
+    points = obstacle.points
+    
+    a, b = tee(np.vstack([np.array(points), points[0]]))
+    next(b, None)
+    
+    for edge in zip(a, b):
+        alpha, beta = _intersect(edge, initial_position, final_position)
+        if alpha <= 1 and alpha >= 0 and beta >= 0 and beta <= 1:
+            return True
+    return False
+
+def _intersect(edge, initial_position, final_position):
+    displacement = np.array(final_position) - np.array(initial_position)
+    edge_segment = np.array(edge[1])-np.array(edge[0])
+    b = edge[0] - initial_position
+
+    A = np.stack([displacement, edge_segment], axis=1)
+    try:
+        coeff = np.linalg.solve(A, b)
+        alpha, beta = coeff[0], coeff[1]
+    except np.linalg.LinAlgError:
+        alpha, beta = np.float('inf'), np.float('inf')
+    return alpha, beta
+
+def termination(goal_position, std_dev=0.001):
+    """
+        Factory for termination probability.
+    """
+    def __termination(state):
+        if isinstance(std_dev, float):
+            z = np.linalg.norm(state-goal_position)/std_dev
+        else:
+            z = np.linalg.norm((state-goal_position)/std_dev)
+        tail_prob = 1-erf(z/np.sqrt(2))
+        return tail_prob
+    return __termination
+
+def termination_velocity(std_dev=0.001):
+    """
+        Factory for termination probability when velocity is close to zero
+    """
+    def __termination(state):
+        if isinstance(std_dev, float):
+            z = np.linalg.norm(state[..., 2:])/std_dev
+        else:
+            z = np.linalg.norm((state[..., 2:])/std_dev)
+        tail_prob = 1-erf(z/np.sqrt(2))
+        return tail_prob
+    return __termination
+
+### Controllers.
+
+class PID:
+    def __init__(self, kp, ki=0., kd=0.):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.prev_error = 0.
+        self.integral_error = 0.
+    
+    def __call__(self, error):
+        self.integral_error += error
+        derivative_error = self.prev_error - error
+        self.prev_error = error
+        return self.kp * error + self.ki * self.integral_error + self.kd * derivative_error
+
+
+ACC_X = 0
+ACC_Y = 1
+DEC_X = 2
+DEC_Y = 3
+ACC_NONE = 4
+
+def position_controller_discrete(goal, kp_vel, ki_vel, kp_pos, kd_pos):
+    """
+        Factory for controller to achive goal position
+    """
+    controller_pos = PID(kp=kp_pos, ki= 1, kd=kd_pos)
+    controller_vel = PID(kp=kp_vel, ki=ki_vel)
+    actions = [[DEC_X, ACC_X], [DEC_Y, ACC_Y]]
+    def __controller(state): 
+        error = goal - state
+        u = controller_pos(error[:2]) + controller_vel(error[2:]) # velocity control signal
+        if np.linalg.norm(u) < 1/5: 
+            return ACC_NONE # do nothing
+        else:
+            # probs = softmax(np.abs(u))
+            # sample = np.random.choice([0,1], p=probs) # select axis to act
+            sample = np.argmax(np.abs(u)) # select axis
+            return actions[sample][int(u[sample]>=0)]
+            
+    return __controller
+
+def position_controller_continuous(goal, kp_vel, ki_vel, kp_pos, kd_pos):
+    """
+        Factory for controller to achive goal position
+    """
+    controller_pos = PID(kp=kp_pos, ki= 1, kd=kd_pos)
+    controller_vel = PID(kp=kp_vel, ki=ki_vel)
+    MAX_SPEED = 0.99
+    def __controller(state): 
+        error = goal - state
+        u = controller_pos(error[:2]) + controller_vel(error[2:]) # velocity control signal
+        return u.clip(-MAX_SPEED, MAX_SPEED)
+            
+    return __controller
+
+def position_controller_factory(init_state, distance, continuous=True):
+    return position_controller_continuous(init_state + distance, 5, 0.01, 100, 0.) if continuous else position_controller_discrete(init_state+distance, 10, 0.1, 100, 0.)
+
+def create_position_controllers(env, translation_distance=1/10):
+    position_options = []
+    controller_factory = position_controller_factory
+    std_dev_vel = 0.01
+    for y in [-1., 1.]:
+        o = Option(initiation_set(env, np.array([0., y*translation_distance])),
+               partial(controller_factory, distance=np.array([0., y*translation_distance, 0., 0.]), continuous=isinstance(env.action_space, spaces.Box)),
+               termination_velocity(std_dev=std_dev_vel),
+               name=f"{'+' if y > 0 else '-'}Y"
+        )
+        position_options.append(o)
+    for x in [-1., 1.]:
+        o = Option(initiation_set(env, np.array([x*translation_distance, 0.])),
+               partial(controller_factory, distance=np.array([x*translation_distance, 0., 0., 0.]), continuous=isinstance(env.action_space, spaces.Box)),
+               termination_velocity(std_dev=std_dev_vel),
+               name=f"{'+' if x > 0 else '-'}X"
+        )
+        position_options.append(o)
+    
+    
+    return position_options
+    
+
---
+++ envs/pinball/pinball_gym.py
@@ -0,0 +1,250 @@
+"""
+    OpenAI Gym Adapter for the Pinball Domain
+    author: Rafael Rodriguez-Sanchez
+    date: October 2022 
+    email: rrs@brown.edu
+"""
+import gym
+from gym import spaces
+from gym.error import DependencyNotInstalled
+
+import numpy as np
+from src.envs.pinball.pinball import PinballModel, PinballView, BallModel
+
+from collections import UserDict, deque
+from matplotlib.path import Path
+
+
+class GoalPinballModel(PinballModel):
+    def set_target_pos(self, target_pos):
+        self.target_pos = target_pos
+
+    def set_initial_pos(self, start_pos):
+        self.ball = BallModel(start_position=start_pos, radius=0.01)
+    
+    def set_initial_state(self, state):
+        self.set_initial_pos(state[:2])
+        self.ball.add_impulse(state[2]*5, state[3]*5)
+
+
+
+class PinballEnv(gym.Env):
+    metadata = {
+        "render_modes": ["human", "rgb_array"],
+        "render_fps": 30,
+    }
+    
+    def __init__(self, config, start_pos=None, target_pos=None, width=500, height=500, render_mode=None):
+        self.action_space = spaces.Discrete(5)
+        self.observation_space = spaces.Box(low=np.zeros(4), high=np.ones(4))
+        self.gamma = 1
+        self.configuration = config
+        self.width = width
+        self.height = height
+        self.render_mode = render_mode
+        self.screen = None  
+        self.clock = None
+
+        self.pinball = GoalPinballModel(self.configuration)
+        self.state = self.pinball.get_state()
+        self._obstacles = [Path(obstacle.points) for obstacle in self.pinball.obstacles]
+        if start_pos:
+            self.pinball.set_initial_pos(start_pos)
+        if target_pos:
+            self.pinball.set_target_pos(target_pos)
+
+
+    def step(self, action):
+        assert self.action_space.contains(
+            action
+        ), f"{action!r} ({type(action)}) invalid, action_space {self.action_space}"
+
+        reward = self.pinball.take_action(action)
+        next_state = self.pinball.get_state()
+        done = self.pinball.episode_ended()
+
+        if self.render_mode == "human":
+            self.render()
+
+        return np.array(next_state), reward, done, False, {}
+
+
+    def get_obstacles(self):
+        """
+            return list of Pinball Obstacles
+        """
+        return self.pinball.obstacles
+
+    
+    def sample_initial_positions(self, N):
+        """
+            Samples initial positions uniformly with velocity 0.
+        """
+        all_points = []
+        total_points = 0
+        vels = np.zeros((N, 2))
+        while total_points < N:
+            points = self._get_points_outside_obstacles(np.random.uniform(size=(N, 2)))
+            all_points.append(points)
+            total_points += points.shape[0]
+        return np.hstack([np.vstack(all_points)[:N], vels])
+
+    def sample_init_states(self, N):
+        """
+            Samples states uniformly 
+        """
+        all_points = []
+        total_points = 0
+        vels = np.random.uniform(size=(N, 2))
+        while total_points < N:
+            points = self._get_points_outside_obstacles(np.random.uniform(size=(N, 2)))
+            all_points.append(points)
+            total_points += points.shape[0]
+        return np.hstack([np.vstack(all_points)[:N], vels])
+
+    def _get_points_outside_obstacles(self, points):
+        points_mask = np.ones(points.shape[0], dtype=np.bool8)
+        for obstacle in self._obstacles:
+            points_mask = np.logical_and(np.logical_not(obstacle.contains_points(points, radius=0.05)), points_mask)
+
+        return points[points_mask]
+
+    def reset(self, state=None):
+        self.pinball = PinballModel(self.configuration) 
+        return np.array(self.pinball.get_state())
+
+    def render(self):
+        if self.render_mode is None:
+            gym.logger.warn(
+                "You are calling render method without specifying any render mode. "
+                "You can specify the render_mode at initialization, "
+                f'e.g. gym("{self.spec.id}", render_mode="rgb_array")'
+            )
+            return
+
+        try:
+            import pygame
+        except ImportError:
+            raise DependencyNotInstalled(
+                "pygame is not installed, run `pip install gym[classic_control]`"
+            )
+        
+        if self.screen is None:
+            if self.render_mode == "human":
+                pygame.init()
+                pygame.display.set_caption('Pinball Domain')
+                self.screen = pygame.display.set_mode([self.width, self.height])
+                self.environment_view = PinballView(self.screen, self.pinball)      
+            else:
+                self.screen = pygame.Surface((self.width, self.height))
+                self.environment_view = PinballView(self.screen, self.pinball)  
+
+        if self.clock is None:
+            self.clock = pygame.time.Clock()
+
+        if self.render_mode == "human":
+            self.environment_view.blit()
+            pygame.event.pump()
+            self.clock.tick(self.metadata["render_fps"])
+            pygame.display.flip()
+
+        elif self.render_mode == "rgb_array":
+            self.environment_view.blit()
+            return np.transpose(
+                np.array(pygame.surfarray.pixels3d(self.screen))/255, axes=(1, 0, 2)
+            )
+
+class DummyDict(UserDict):
+    def __init__(self, default_value_f):
+        self.default_value_f = default_value_f
+        self.data = []
+    
+    def __missing__(self, key):
+        return self.default_value_f(key)
+
+class PinballModelContinuous(GoalPinballModel):
+    MAX_SPEED = 1
+    
+    def __init__(self, configuration):
+        super().__init__(configuration)
+        self.action_effects = DummyDict(self._action_effects)
+
+    def _action_effects(self, action):
+        _action = np.clip(action, -self.MAX_SPEED, self.MAX_SPEED)
+        return _action[0], _action[1]
+
+
+class PinballEnvContinuous(PinballEnv):
+    MAX_SPEED = 1
+    def __init__(self, config, start_pos=None, target_pos=None, width=500, height=500, render_mode=None):
+        super().__init__(config, start_pos, target_pos, width, height, render_mode)
+        self.action_space = spaces.Box(low=-self.MAX_SPEED, high=self.MAX_SPEED, shape=(2,), dtype=np.float64)
+        self.pinball = PinballModelContinuous(config)
+    
+    def reset(self, state=None):
+        if state is not None:
+            self.pinball.set_initial_state(state)
+        return np.array(self.pinball.get_state())
+
+    def step(self, action):
+        action = tuple(action)
+        return super().step(action)
+
+
+class PinballPixelWrapper(gym.Env):
+    def __init__(self, environment, n_frames=1):
+        self.frames = deque(maxlen=n_frames)
+        self.env = environment
+        self.n_frames = n_frames
+
+    def step(self, *args, **kwargs):
+        ret = self.env.step(*args, **kwargs)
+        frame = self.env.render()
+        if len(self.frames) < self.n_frames:
+            self._init_queue(frame)
+        self.frames.append(frame)
+
+        return np.array(self.frames), *ret[1:-1], {"next_state": ret[0]}
+
+    def _init_queue(self, frame):
+        for i in range(self.n_frames):
+            self.frames.append(np.zeros_like(frame))
+
+    def reset(self, *args, **kwargs):
+        self.frames.clear()
+        self.env.reset(*args, **kwargs)
+        frame = self.env.render()
+        if len(self.frames) < self.n_frames:
+            self._init_queue(frame)
+        self.frames.append(frame)
+        return np.array(self.frames)
+
+    def sample_initial_positions(self, N):
+        return self.env.sample_initial_positions(N)
+    
+    @property
+    def action_space(self):
+        return self.env.action_space
+
+    def get_obstacles(self):
+        return self.env.get_obstacles()
+
+if __name__=="__main__":
+    from matplotlib import pyplot as plt
+    size = 100
+    pinball = PinballEnvContinuous(
+                                    config="/Users/rrs/Desktop/abs-mdp/src/envs/pinball/configs/pinball_simple_single.cfg", 
+                                    width=size, 
+                                    height=size, 
+                                    render_mode="rgb_array")
+    pixel_pinball = PinballPixelWrapper(pinball, n_frames=5)
+    pinball.reset([0.5, 0.5, 0., 0.])
+    imgs = []
+    for i in range(10):
+        a = [0.9, 0.9]
+        next_s, _, _, _, _ = pinball.step(a)
+        imgs.append(pinball.render())
+        _s = pinball.reset(next_s)
+        
+    plt.imshow(np.array(imgs).mean(0))
+    plt.show()
