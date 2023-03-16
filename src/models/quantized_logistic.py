b0299a53964fd2cabf5a7aa941749ac1b08e86de
Rafael Rodriguez <rrs@brown.edu>
2023-03-16 17:37:59 -0400

    quantize logistic implementation

---
+++ src/models/quantized_logistic.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn.functional as F
+
+from src.utils.printarr import printarr
+class QuantizedLogisticMixture(torch.distributions.Distribution):
+    def __init__(self, loc, log_scale, logit_probs, num_bins=255):
+        super().__init__()
+        self.loc = loc # [batch_size, num_channels, height, width, num_components]
+        self.log_scale = torch.clamp(log_scale, min=-7) # [batch_size, num_channels, height, width, num_components]
+        self.logit_probs = logit_probs # [batch_size, num_channels, height, width, num_components]
+        self.num_bins = num_bins # int
+
+    def log_prob(self, x):
+        # x.shape = [batch_size, num_channels, height, width]
+        # x is in [0, 1]
+        # compute log prob in the bin
+        # assert torch.all(x >= 0) and torch.all(x <= 1)
+        assert torch.all(x >= -1) and torch.all(x <= 1)
+        x = x.unsqueeze(-1) # [batch_size, num_channels, height, width, 1]
+        # x = x.unsqueeze(-1)*2 -1 # rescale to [-1, 1]
+        # printarr(x, self.loc, self.log_scale, self.logit_probs)
+        centered_x = x - self.loc
+        inv_log_scale = torch.exp(-self.log_scale)
+        
+        max_x = (centered_x + 1/self.num_bins) * inv_log_scale
+        cdf_plus = torch.sigmoid(max_x)
+        min_x = (centered_x - 1/self.num_bins) * inv_log_scale
+        cdf_minus = torch.sigmoid(min_x)
+        # left extreme (x=0)
+        log_cdf_left = max_x - F.softplus(max_x)
+        # right extreme (x=255)
+        log_cdf_right = -F.softplus(min_x)
+        log_cdf = torch.log(torch.clamp(cdf_plus - cdf_minus, min=1e-12))
+
+        mid_in = inv_log_scale * centered_x
+        log_pdf_mid = mid_in - self.log_scale - 2. * F.softplus(mid_in) - torch.log(torch.tensor(127.5))
+
+        cond_ = (cdf_plus-cdf_minus < 1e-5).float()
+        log_cdf = cond_ * log_pdf_mid + (1-cond_) * log_cdf
+        left_cond = (x < -0.999).float()
+        log_probs = left_cond * log_cdf_left + (1-left_cond) * log_cdf
+        right_cond = (x > 0.999).float()
+        log_probs = right_cond * log_cdf_right + (1-right_cond) * log_probs
+
+        log_probs = log_probs + torch.log_softmax(self.logit_probs, dim=-1)
+        return torch.sum(torch.logsumexp(log_probs, dim=-1).reshape(x.shape[0], -1), dim=-1)
+        
+
+
+
+
+
+
+        
--- tests/pixelcnn/pixelcnn_mnist_train.py
+++ tests/pixelcnn/pixelcnn_mnist_train.py
@@ -9,6 +9,7 @@ from torchvision import transforms
 
 from src.models import DiagonalGaussianModule
 from src.models import PixelCNNStack, GatedPixelCNNLayer
+from src.models.quantized_logistic import QuantizedLogisticMixture
 from src.utils.printarr import printarr
 import matplotlib.pyplot as plt
 
@@ -51,11 +52,61 @@ class PixelCNNDecoderBinary(pl.LightningModule):
         return loss
    
     def configure_optimizers(self) -> torch.optim.Optimizer:
-        return torch.optim.Adam(self.parameters(), lr=3e-5)
+        return torch.optim.Adam(self.parameters(), lr=1e-4)
 
 
 def conv_out_dim(in_dim, kernel_size, stride, padding=0):
     return int((in_dim - kernel_size + 2 * padding) / stride + 1)
+
+
+class PixelCNNLogistic(pl.LightningModule):
+    '''
+        PixelCNN for binary images (Binarized MNIST)
+    '''
+    def __init__(self, n_classes, in_channels, out_channels, kernel_size=3, n_layers=5):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.n_layers = n_layers
+        self.n_components = 10
+        self.embed = nn.Embedding(n_classes, out_channels)
+        self.causal_block = GatedPixelCNNLayer(in_channels, out_channels, kernel_size, mask_type='A') # causal.
+        self.stack = PixelCNNStack(out_channels, kernel_size, n_layers-1)
+        self.conv = nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1)
+        self.output = nn.Conv2d(out_channels, self.n_components * 3, kernel_size=1, stride=1)
+
+    def forward(self, x, label):
+        conditional = self.embed(label).reshape(x.shape[0],-1, 1, 1)
+        vertical, horizontal, _ = self.causal_block(x, x, None)
+        vertical, horizontal, skip = self.stack(vertical, horizontal, conditional)
+        out = F.relu(skip)
+        return self.output(F.relu(self.conv(out))).permute(0,2,3,1).contiguous()
+
+    def training_step(self, batch, batch_idx):
+        x, label = batch
+        x = x * 2 - 1
+        out = self(x, label).unsqueeze(1) # n_channels
+        log_probs = QuantizedLogisticMixture(out[..., :self.n_components], out[..., self.n_components:2*self.n_components], out[..., 2*self.n_components:]).log_prob(x)
+        # printarr(x, out, idx)
+        # loss = F.nll_loss(out, idx.squeeze(), reduction='none').reshape(x.shape[0], -1).sum(-1).mean()
+        loss = -log_probs.mean()
+        self.log('train_loss', loss)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        x, label = batch
+        x = x * 2 - 1
+        out = self(x, label).unsqueeze(1)
+        log_probs = QuantizedLogisticMixture(out[..., :self.n_components], out[..., self.n_components:2*self.n_components], out[..., 2*self.n_components:]).log_prob(x)
+        # printarr(x, out, idx)
+        # loss = F.nll_loss(out, idx.squeeze(), reduction='none').reshape(x.shape[0], -1).sum(-1).mean()
+        loss = -log_probs.mean()
+        self.log('val_loss', loss, prog_bar=True)
+        return loss
+   
+    def configure_optimizers(self) -> torch.optim.Optimizer:
+        return torch.optim.Adam(self.parameters(), lr=5e-5)
 
 
 # def encoder_conv_continuous(in_width, in_height, in_channels, kernel_size=3, stride=1, hidden_dim=128, latent_dim=2):
@@ -99,14 +150,14 @@ if __name__ == "__main__":
     transform = transforms.Compose([transforms.ToTensor(), transforms.Grayscale(num_output_channels=1)])
     train_dataset = MNIST(root='data', train=True, download=True, transform=transform)
     val_dataset = MNIST(root='data', train=False, download=True, transform=transform)
-    train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True)
+    train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=128, shuffle=False)
 
 
     # Train
-    model = PixelCNNDecoderBinary(n_classes=10, in_channels=1, out_channels=64, kernel_size=7, n_layers=5)
+    model = PixelCNNLogistic(n_classes=10, in_channels=1, out_channels=64, kernel_size=7, n_layers=5)
 
-    trainer = pl.Trainer(max_epochs=2, accelerator='gpu')
+    trainer = pl.Trainer(max_epochs=10, accelerator='gpu', overfit_batches=0.5)
     trainer.fit(model, train_loader, val_loader)
 
     # Save model
